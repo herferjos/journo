757d94f23946c2dcaf29321fd9302afaa804afc5
herferjos <120600448+herferjos@users.noreply.github.com>
2024-01-18 18:27:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,6 @@ if 'autenticado' in st.session_state:
         st.info("✅ Aquí tienes la transcripción de tu audio. Si quieres puedes seleccionar fragmentos de ella para indicar que partes son más importantes a la hora de generar la noticia.")
 
         st.session_state.anotaciones = text_highlighter(st.session_state.transcription)
-        st.write(st.session_state.anotaciones)
         if st.button("Generar noticia", type = "primary"):
           with st.spinner("Generando noticia... ⌛"):
             
@@ -87,6 +86,8 @@ if 'autenticado' in st.session_state:
                 anotaciones.append(item['label'])
                                    
             st.session_state.anotaciones = anotaciones
+
+            st.write(st.session_state.anotaciones)
               
             st.session_state.noticia_generada = generar_noticia(st.session_state.transcription, st.session_state.anotaciones, st.session_state.X, st.session_state.Y, st.session_state.Z, st.session_state.A, st.session_state.B)
         
