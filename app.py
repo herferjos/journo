af221caad22d8396966313557416ba024106c97f
herferjos <120600448+herferjos@users.noreply.github.com>
2024-01-19 10:00:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,19 +77,19 @@ if 'autenticado' in st.session_state:
     if 'transcription2' in st.session_state and 'noticia_generada' not in st.session_state:
         st.info("✅ Aquí tienes la transcripción de tu audio. Si quieres puedes seleccionar fragmentos de ella para indicar que partes son más importantes a la hora de generar la noticia.")
         st.session_state.anotaciones = text_highlighter(st.session_state.transcription2)
-          if st.button("Generar noticia", type = "primary"):
-            with st.spinner("Generando noticia... ⌛"):
-              
-              anotaciones = []
-              
-              for elemento in st.session_state.anotaciones:
-                for item in elemento:
-                  anotaciones.append(item['label'])
-                                     
-              st.session_state.anotaciones = anotaciones
-              st.session_state.noticia_generada = generar_noticia(st.session_state.transcription2, st.session_state.anotaciones, st.session_state.X, st.session_state.Y, st.session_state.Z, st.session_state.A, st.session_state.B)
-              
-              st.rerun()
+        if st.button("Generar noticia", type = "primary"):
+          with st.spinner("Generando noticia... ⌛"):
+            
+            anotaciones = []
+            
+            for elemento in st.session_state.anotaciones:
+              for item in elemento:
+                anotaciones.append(item['label'])
+                                   
+            st.session_state.anotaciones = anotaciones
+            st.session_state.noticia_generada = generar_noticia(st.session_state.transcription2, st.session_state.anotaciones, st.session_state.X, st.session_state.Y, st.session_state.Z, st.session_state.A, st.session_state.B)
+            
+            st.rerun()
               
 
     if 'noticia_generada' in st.session_state:
