8755207be30a4932a4ef63cdc1c9ecf7d1554719
herferjos <120600448+herferjos@users.noreply.github.com>
2024-01-22 22:21:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,35 @@ from openai import OpenAI
 import streamlit as st
 from modules import *
 from streamlit_annotation_tools import text_highlighter
-from audio_recorder_streamlit import audio_recorder
+
+
+from streamlit_mic_recorder import mic_recorder,speech_to_text
+
+state=st.session_state
+
+if 'text_received' not in state:
+    state.text_received=[]
+
+c1,c2=st.columns(2)
+with c1:
+    st.write("Convert speech to text:")
+with c2:
+    text=speech_to_text(language='en',use_container_width=True,just_once=True,key='STT')
+
+if text:       
+    state.text_received.append(text)
+
+for text in state.text_received:
+    st.text(text)
+
+st.write("Record your voice, and play the recorded audio:")
+audio=mic_recorder(start_prompt="⏺️",stop_prompt="⏹️",key='recorder')
+
+if audio:       
+    st.audio(audio['bytes'])
+
+
+
 
 st.set_page_config(page_title="Journo.AI", page_icon="🗞️", layout="wide")
 
