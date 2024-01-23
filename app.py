26053625bb9b8a1324e81791aecdaf382ee0edf4
herferjos <120600448+herferjos@users.noreply.github.com>
2024-01-23 16:40:57 +0100

    update

--- app.py
+++ app.py
@@ -86,19 +86,19 @@ if 'autenticado' in st.session_state:
 
           for i in range(len(lista_claves)):
             st.write(f"### {lista_claves[i]}")
-              with st.expander('Ver diálogos'):
-                  texto = '\n\n- '.join(st.session_state.new_dialogos[lista_claves[i]])
-                  texto = '- ' + texto
-                  
-                  patron = r'- (.+):'
-                  coincidencias = re.findall(patron, texto)
-                  
-                  for elemento in coincidencias:
-                      texto_formateado = f'<u><b>{elemento}</u></b>'
-                      texto = re.sub(f'- {elemento}:', f'- {texto_formateado}:', texto)      
-                            
-                  # Mostrar el texto formateado
-                  st.write(texto, unsafe_allow_html=True)    
+            with st.expander('Ver diálogos'):
+                texto = '\n\n- '.join(st.session_state.new_dialogos[lista_claves[i]])
+                texto = '- ' + texto
+                
+                patron = r'- (.+):'
+                coincidencias = re.findall(patron, texto)
+                
+                for elemento in coincidencias:
+                    texto_formateado = f'<u><b>{elemento}</u></b>'
+                    texto = re.sub(f'- {elemento}:', f'- {texto_formateado}:', texto)      
+                          
+                # Mostrar el texto formateado
+                st.write(texto, unsafe_allow_html=True)    
 
     if 'anotaciones' in st.session_state and not 'noticia_generada' in st.session_state:
         audio, transcripcion, topics, annotation = st.tabs(["Audio", "Transcripción", "Temas seleccionados", "Anotaciones"])
