33dba49c2ce84ecfa8a1c2d880a9b42ed1eb780d
herferjos <120600448+herferjos@users.noreply.github.com>
2024-01-23 17:40:20 +0100

    update

--- app.py
+++ app.py
@@ -357,7 +357,8 @@ if 'autenticado' in st.session_state:
           with st.spinner("Generando noticia... ⌛"):
 
             st.session_state.noticia_generada = generar_noticia(st.session_state.new_dialogos, st.session_state.anotaciones, st.session_state.X, st.session_state.Y, st.session_state.Z, st.session_state.A, st.session_state.B)
-          
+            st.rerun()
+            
     if 'noticia_generada' in st.session_state:
         st.write("""## ✔️¡Listo! Aquí tienes tu noticia:""")
 
