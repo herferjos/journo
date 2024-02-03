2914e34a691e7f808fa52bc6999cc3d361acfbdf
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-03 18:39:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ if 'email' in st.session_state and st.session_state.user_subscribed == True:
       
         exec(content)
       
-        phase = stx.stepper_bar(steps=["Audio", "Contexto", "Selección temática", "Destacar momentos", "Noticia"])
+        phase = stx.stepper_bar(steps=["Audio", "Contexto", "Transcripción", "Selección/descarte", "Noticia"])
       
         if phase == 0:
             st.write("### 1) Cargar o subir audio")
@@ -165,33 +165,28 @@ if 'email' in st.session_state and st.session_state.user_subscribed == True:
                 st.write(st.session_state.A)
                 st.write("#### :blue[Cuándo ha dicho las declaraciones?]")
                 st.write(st.session_state.B)
-        
+                
         if phase == 2:
-            st.write("### 3) Selección/descarte de temas mencionados")
-            st.info("A continuación deberemos deseleccionar aquellos asuntos que no queremos incluir en la noticia final y fueron mencionados en las declaraciones.")
-            with st.expander("*Ver ejemplo*"):
-              for i in range(len(st.session_state.topics)):
-                  st.session_state[f'on_{st.session_state.topics[i]}'] = st.toggle(st.session_state.topics[i], key=f"{st.session_state.topics[i]}", value = True)
+            st.write("### 3) Transcripción de las declaraciones")
+            st.info("Aquí tienes la transcripción del audio completa")
+            st.write(st.session_state.transcription2, unsafe_allow_html=True)
+        
             
-                  with st.expander('Ver diálogos'):
-                      texto = '\n\n- '.join(st.session_state.dialogos_topics[st.session_state.topics[i]])
-                      texto = '- ' + texto
-                      
-                      patron = r'- (.+):'
-                      coincidencias = re.findall(patron, texto)
-                      
-                      for elemento in coincidencias:
-                          texto_formateado = f'<u><b>{elemento}</u></b>'
-                          texto = re.sub(f'- {elemento}:', f'- {texto_formateado}:', texto)      
-                                
-                      # Mostrar el texto formateado
-                      st.write(texto, unsafe_allow_html=True) 
-
-
-
-
-
-
+        if phase == 3:
+          st.write("### 4) Selección/descarte de temas mencionados")
+          st.info("Aquí tienes los párrafos descartados (aparecen desmarcados) y los momentos de mayor relevancia en las declaraciones.")
+            
+          for i in range(len(st.session_state.lista)):
+              on = st.toggle('', key=i, value = st.session_state[f'on_{i}'])
+              frases = []
+              for item in st.session_state[f'anotaciones_{i}']:
+                  for x in item:
+                    frases.append(x['label'])
+              st.write(generar_html_con_destacados(st.session_state.lista[i], frases), unsafe_allow_html=True)
+              
+        if phase == "4":
+            st.info('Esta es la noticia generada por Journo')
+            st.write(st.session_state.noticia_generada)
 
 
 
