4a9660849fc11b59007644635e8ac713c9ccb5ab
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-03 12:14:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,6 @@ from rsc.aggregate_auth import add_auth
 
 st.set_page_config(page_title="Journo", page_icon="🗞️", layout="wide")
 
-hide_st_style = """
-<style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-header {visibility: hidden;}
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-.styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-.viewerBadge_text__1JaDK {
-    display: none;
-}
-</style>
-"""
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
 st.write(hide_st_style)
 
 st.markdown(
