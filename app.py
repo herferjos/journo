dc6d48bddd9bc3c9d2ee2d5edf016c7d118189a8
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-03 16:43:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if 'email' in st.session_state and st.session_state.user_subscribed == True:
     if 'database' not in st.session_state:
       st.session_state.sheet = load_sheet()
       try:
-        st.session_state.database = conn.read(worksheet=st.session_state.email)
+        st.session_state.database = st.session_state.sheet.read(worksheet=st.session_state.email)
       except:
         nuevo_df = pd.DataFrame({'Transcripción': [None]*5, 'Cargo': [None]*5, 'Nombre': [None]*5, 'Tema': [None]*5, 'Donde': [None]*5, 'Cuando': [None]*5, 'Transcripción filtrada': [None]*5, 'Anotaciones': [None]*5, 'Noticia': [None]*5}, index=range(5))
         st.session_state.sheet.create(worksheet=st.session_state.email,data=nuevo_df)
