6150802652ba9bbbcb75aecee446031b800c9170
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-03 17:12:53 +0100

    Update modules.py

--- rsc/modules.py
+++ rsc/modules.py
@@ -97,7 +97,7 @@ def dataframetipo(df):
           elemento.pop('_selectedRowNodeInfo')
       df = pd.DataFrame(selected_row_dict)
         
-      return df, selected_row[0]['nodeId']
+      return df, selected_row
 
 def img_to_bytes(img_path):
     img_bytes = Path(img_path).read_bytes()
