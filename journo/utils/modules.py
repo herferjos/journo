1b26e61e6d0261c4c8023b99e868cb5fea1bc969
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-06 21:54:41 +0100

    Update modules.py

--- journo/utils/modules.py
+++ journo/utils/modules.py
@@ -19,7 +19,7 @@ from streamlit_gsheets import GSheetsConnection
 # Configuración de la clave API de OpenAI
 openai_client = OpenAI(api_key=st.secrets.openai_api)
 
-def load_database()
+def load_database():
     if 'database' not in st.session_state:
       st.session_state.sheet = load_sheet()
       try:
