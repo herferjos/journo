03bf152055fdbc1b035a61ecc8268c48f6a74854
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-06 21:54:24 +0100

    Update modules.py

--- journo/utils/modules.py
+++ journo/utils/modules.py
@@ -19,6 +19,16 @@ from streamlit_gsheets import GSheetsConnection
 # Configuración de la clave API de OpenAI
 openai_client = OpenAI(api_key=st.secrets.openai_api)
 
+def load_database()
+    if 'database' not in st.session_state:
+      st.session_state.sheet = load_sheet()
+      try:
+        st.session_state.database = st.session_state.sheet.read(worksheet=st.session_state.email)
+      except:
+        nuevo_df = pd.DataFrame({'Transcripción': [None]*5, 'Cargo': [None]*5, 'Nombre': [None]*5, 'Tema': [None]*5, 'Donde': [None]*5, 'Cuando': [None]*5, 'Transcripción filtrada': [None]*5, 'Anotaciones': [None]*5, 'Noticia': [None]*5, 'Sesion': [None]*5}, index=range(5))
+        st.session_state.sheet.create(worksheet=st.session_state.email,data=nuevo_df)
+        st.session_state.database = st.session_state.sheet.read(worksheet=st.session_state.email)
+    return
 
 def reset_variables():
     to_delete = [key for key in st.session_state.keys() if key.startswith('on') or key.startswith('anotaciones')]
