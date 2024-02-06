04a019c44a838a0ea12fcc18be76253d103cb847
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-06 18:30:27 +0100

    Update journo.py

--- journo/pages/journo.py
+++ journo/pages/journo.py
@@ -9,7 +9,7 @@ import pandas as pd
 def show_journo():
     st.write('## ✍🏼 Redacta con Journo')
     
-    st.session_state.phase = stx.stepper_bar(steps=["Audio", "Contexto", "Transcripción", "Selección/descarte", "Noticia generada", "Chatear con IA", "Enviar información"])
+    st.session_state.phase = stx.stepper_bar(steps=["Audio", "Contexto", "Transcripción", "Selección/descarte", "Noticia generada"])
 
     a, b, c = st.columns([0.5, 0.3, 0.5])
     
@@ -158,45 +158,5 @@ def show_journo():
                 st.rerun()
         else:
             st.warning('Aún no has generado ninguna noticia. Vuelve al paso anterior y genera la noticia.')
-            
-            
-    if st.session_state.phase == 5:
-        st.write('## 🤖 Chatea con una IA y ayúdate')
-        st.info('Puedes chatear con una IA para ayudarte a formatear la noticia cómo desees. Además, podrás importar fácilmente la noticia de la sección "Noticia generada" haciendo click en el siguiente botón:')
-        if st.button("Copiar noticia ", type = "primary"):
-            st.session_state.messages.append({"role": "system", "content": f"Esta es la noticia del usuario: {st.session_state.noticia_generada}"})
-        
-        for message in st.session_state.messages:
-            if message["role"] == "system":
-                pass
-            else:
-                with st.chat_message(message["role"]):
-                    st.markdown(message["content"])
-        
-        prompt = st.chat_input("Pregunta lo que quieras")
-
-        if st.button("Enviar ", type = "primary"):
-            
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            with st.chat_message("user"):
-                st.markdown(prompt)
-                
-            with st.chat_message("assistant"):
-                        
-                response = openai_client.chat.completions.create(
-                model="gpt-3.5-turbo-1106",
-                messages=st.session_state.messages,
-                temperature = 0,
-                stream = True
-                )
-                
-                message_placeholder = st.empty()
-                full_response = ""
-                
-                for chunk in response:
-                    full_response += chunk.choices[0].delta.get("content", "")
-                    message_placeholder.markdown(full_response + "▌")
-                          
-                st.session_state.messages.append({"role": "assistant", "content": full_response})
 
     return
