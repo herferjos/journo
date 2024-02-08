3006ca7da83f4e3c93a899553cc0d7b1c5d178c9
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-08 17:15:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,6 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.write("---")
-
 
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "system", "content": "Eres un asistente virtual de Journo, una webapp de asistencia con IA para periodistas y ahora podrás comunicarte con los usuarios de Journo. Trata de ayudar a los usuarios con sus peticiones e instrucciones para dar forma y estilo a una noticia periodística. Razona siempre paso por paso cualquier petición."}]
