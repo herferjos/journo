149a92f545c163a170846c646b0a443a69bdefe8
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-11 19:27:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,63 +7,62 @@ from journo.utils.modules import img_to_html, load_database
 from streamlit_option_menu import option_menu
 import time
 
-try:
-    st.set_page_config(page_title="Journo", page_icon="🗞️") #layout="wide"
-    
-    st.markdown(
-        "<p style='text-align: center; color: grey;'>" + img_to_html('files/logo.png', 200, 200) + "</p>",
-        unsafe_allow_html=True
-    )
-    
-    st.markdown(
-        """
-        <div style='text-align: center;'>
-            <h4>Tu asistente periodístico de inteligencia artificial</h4>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-    
-    
-    if "messages" not in st.session_state:
-        st.session_state.messages = [{"role": "system", "content": "Eres un asistente virtual de Journo, una webapp de asistencia con IA para periodistas y ahora podrás comunicarte con los usuarios de Journo. Trata de ayudar a los usuarios con sus peticiones e instrucciones para dar forma y estilo a una noticia periodística. Razona siempre paso por paso cualquier petición."}]
-    
-    if 'guardado' not in st.session_state:
-        st.session_state.guardado = False
-        
-    if 'noticia_cargada' not in st.session_state:
-        st.session_state.noticia_cargada = False
-    
-    
-    st.markdown("""
-      <style>
-      div.stLinkButton {text-align:center}
-      </style>""", unsafe_allow_html=True)
-    
-    
-    add_auth(required=True, login_sidebar = False)
-    
-    load_database()
-    
-    with st.sidebar:
-        st.session_state.selected = option_menu("", ["Crea tu noticia", "Chatbot", "¿Qué es Journo?"], 
-            icons=['pencil-fill', 'robot', 'house'], menu_icon="", default_index=0)
-    
-    if 'email' in st.session_state and st.session_state.user_subscribed == True: 
-        
-        if st.session_state.selected == 'Crea tu noticia':
-            show_journo()
-    
-        if st.session_state.selected == 'Chatbot':
-            show_bot()
-        
-        if st.session_state.selected == '¿Qué es Journo?':
-            show_inicio()
-    
+st.set_page_config(page_title="Journo", page_icon="🗞️") #layout="wide"
 
-except:
+st.markdown(
+    "<p style='text-align: center; color: grey;'>" + img_to_html('files/logo.png', 200, 200) + "</p>",
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <div style='text-align: center;'>
+        <h4>Tu asistente periodístico de inteligencia artificial</h4>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "system", "content": "Eres un asistente virtual de Journo, una webapp de asistencia con IA para periodistas y ahora podrás comunicarte con los usuarios de Journo. Trata de ayudar a los usuarios con sus peticiones e instrucciones para dar forma y estilo a una noticia periodística. Razona siempre paso por paso cualquier petición."}]
+
+if 'guardado' not in st.session_state:
+    st.session_state.guardado = False
+    
+if 'noticia_cargada' not in st.session_state:
+    st.session_state.noticia_cargada = False
+
+
+st.markdown("""
+  <style>
+  div.stLinkButton {text-align:center}
+  </style>""", unsafe_allow_html=True)
+
+
+add_auth(required=True, login_sidebar = False)
+
+load_database()
+
+with st.sidebar:
+    st.session_state.selected = option_menu("", ["Crea tu noticia", "Chatbot", "¿Qué es Journo?"], 
+        icons=['pencil-fill', 'robot', 'house'], menu_icon="", default_index=0)
+
+if 'email' in st.session_state and st.session_state.user_subscribed == True: 
+    
+    if st.session_state.selected == 'Crea tu noticia':
+        show_journo()
+
+    if st.session_state.selected == 'Chatbot':
+        show_bot()
+    
+    if st.session_state.selected == '¿Qué es Journo?':
+        show_inicio()
+
+
+"""except:
     st.error('Ha habido un error en Journo. La página será recargada en 5 segundos, si el error persiste contácta con hola@journo-ai.com')
     time.sleep(5)
-    st.rerun()
+    st.rerun()"""
         
     
