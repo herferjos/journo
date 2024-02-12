162cdd15126a097fc41b4fbeadb3ab8828b0d81a
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-12 17:38:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,12 +69,10 @@ if st.button('Crear nuevo df'):
     nuevo_df = pd.DataFrame({'Transcripción': ['_']*5, 'Transcripción editada': ['_']*5, 'Cargo': ['_']*5, 'Nombre': ['_']*5, 'Donde': ['_']*5, 'Cuando': ['_']*5, 'Extra': ['_']*5, 'Anotaciones': ['_']*5, 'Noticia': ['_']*5, 'Noticia editada': ['_']*5,'Sesion': ['_']*5}, index=range(5))
     st.session_state.sheet.create(worksheet='test', data=nuevo_df)
     st.session_state.database = st.session_state.sheet.read(worksheet='test')
-    st.rerun()
-
+    
 if st.button('Actualizar'):
     st.session_state.database = st.session_state.database.append({'Transcripción': 'xd', 'Transcripción editada': 'xd', 'Cargo': 'xd', 'Nombre': 'xd', 'Donde': 'xd', 'Cuando': 'xd', 'Extra': 'xd', 'Anotaciones':'xd', 'Noticia': 'xd', 'Noticia editada': 'xd', 'Sesion': 'xd'}, ignore_index=True)
     st.session_state.database = st.session_state.sheet.update(worksheet='test', data = st.session_state.database)
-    st.rerun()
 
 with st.sidebar:
     st.session_state.selected = option_menu("", ["Crea tu noticia", "Chatbot", "¿Qué es Journo?"], 
