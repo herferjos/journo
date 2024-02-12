7a22be32e685e18b75d66eea615786398fef767e
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-12 17:55:39 +0100

    Update modules.py

--- journo/utils/modules.py
+++ journo/utils/modules.py
@@ -20,6 +20,7 @@ from streamlit_gsheets import GSheetsConnection
 openai_client = OpenAI(api_key=st.secrets.openai_api)
 
 def load_database():
+  st.cache_data.clear()
   st.session_state.sheet = load_sheet()
   try:
     st.session_state.database = st.session_state.sheet.read(worksheet=st.session_state.email)
@@ -120,6 +121,7 @@ def guardar_info():
         else:
             st.session_state.database = st.session_state.database.append({'Transcripción': st.session_state.transcription2, 'Transcripción editada': st.session_state.transcripcion_editada, 'Cargo': st.session_state.X, 'Nombre': st.session_state.Y, 'Donde': st.session_state.A, 'Cuando': st.session_state.B, 'Extra': st.session_state.Z, 'Anotaciones': st.session_state.anotaciones_finales, 'Noticia': st.session_state.noticia_generada, 'Noticia editada': st.session_state.noticia_editada, 'Sesion': contenido}, ignore_index=True)
             st.session_state.database = st.session_state.sheet.update(worksheet=st.session_state.email, data = st.session_state.database)
+        st.cache_data.clear()
     return
     
 
