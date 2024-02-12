030632d027161d0d538c41635ae0902a4a02435c
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-12 17:11:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -71,9 +71,9 @@ if st.button('Crear nuevo df'):
 
 if st.button('Actualizar'):
     st.session_state.database.append({'Transcripción': 'xd', 'Transcripción editada': 'xd', 'Cargo': 'xd', 'Nombre': 'xd', 'Donde': 'xd', 'Cuando': 'xd', 'Extra': 'xd', 'Anotaciones':'xd', 'Noticia': 'xd', 'Noticia editada': 'xd', 'Sesion': 'xd'}, ignore_index=True)
-    st.session_state.sheet.update(worksheet='test', data = st.session_state.database)
+    st.session_state.database = st.session_state.sheet.update(worksheet='test', data = st.session_state.database)
     st.session_state.database = st.session_state.sheet.read(worksheet='test')
-
+    st.cache_data.clear()
     st.rerun()
 
 with st.sidebar:
