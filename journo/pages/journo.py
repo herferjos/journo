801ecc6b76cecb791b999831be7ead12a97a5b90
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-13 16:19:47 +0100

    Update journo.py

--- journo/pages/journo.py
+++ journo/pages/journo.py
@@ -7,34 +7,6 @@ import pandas as pd
 import subprocess 
 
 def show_journo():
-    with st.expander('**Ver tus noticias**'):
-        st.write('## 📊 Tus noticias')
-        if st.session_state.database.isna().all().all():
-            st.info('Actualmente no has generado ninguna noticia. Adelante, prueba Journo y guarda tu primera noticia asistida por IA')
-
-            if st.button("Crear nueva noticia", type = "primary", key = "start"):
-                st.warning('¿Estás seguro de que quieres comenzar a crear una nueva noticia desde cero? Perderás la noticia que estás editando ahora mismo')
-                if st.button("¡Sí, adelante!", type = "primary", key = "yes"): 
-                    reset_variables()
-        
-        else:
-            st.info('Aquí tienes las noticias que has generado con el asistente Journo. Puedes cargar una noticia directamente, explorar la información o crear una nueva.')
-            df_copia = st.session_state.database.copy()
-            df_copia = df_copia.iloc[:, :-1]
-            st.session_state.index_cargado = dataframetipo(df_copia)
-            a,b = st.columns([0.7,1])
-            with a:
-                if st.button("Cargar noticia seleccionada", type = "primary", key = "start"):
-                    cargar_noticia()
-            with b:
-                if st.button("Crear nueva noticia", type = "primary", key = "restart"):
-                    st.warning('¿Estás seguro de que quieres comenzar a crear una nueva noticia desde cero? Perderás la noticia que estás editando ahora mismo')
-                    if st.button("¡Sí, adelante!", type = "primary", key = "yes"): 
-                        reset_variables()
-                    
-            if st.session_state.noticia_cargada == True:
-                
-                st.success(f"👍🏻 Noticia cargada correctamente. Ahora puedes seguir modificando la noticia más abajo.")
     st.session_state.phase = stx.stepper_bar(steps=["Audio", "Contexto", "Transcripción", "Destacado", "Noticia"])
 
     if st.session_state.phase == 0:
@@ -158,4 +130,32 @@ def show_journo():
                 st.session_state.noticia_editada = st.session_state.noticia_generada
                 st.rerun()
                   
+    with st.expander('**Ver tus noticias**'):
+        st.write('## 📊 Tus noticias')
+        if st.session_state.database.isna().all().all():
+            st.info('Actualmente no has generado ninguna noticia. Adelante, prueba Journo y guarda tu primera noticia asistida por IA')
+
+            if st.button("Crear nueva noticia", type = "primary", key = "start"):
+                st.warning('¿Estás seguro de que quieres comenzar a crear una nueva noticia desde cero? Perderás la noticia que estás editando ahora mismo')
+                if st.button("¡Sí, adelante!", type = "primary", key = "yes"): 
+                    reset_variables()
+        
+        else:
+            st.info('Aquí tienes las noticias que has generado con el asistente Journo. Puedes cargar una noticia directamente, explorar la información o crear una nueva.')
+            df_copia = st.session_state.database.copy()
+            df_copia = df_copia.iloc[:, :-1]
+            st.session_state.index_cargado = dataframetipo(df_copia)
+            a,b = st.columns([0.7,1])
+            with a:
+                if st.button("Cargar noticia seleccionada", type = "primary", key = "start"):
+                    cargar_noticia()
+            with b:
+                if st.button("Crear nueva noticia", type = "primary", key = "restart"):
+                    st.warning('¿Estás seguro de que quieres comenzar a crear una nueva noticia desde cero? Perderás la noticia que estás editando ahora mismo')
+                    if st.button("¡Sí, adelante!", type = "primary", key = "yes"): 
+                        reset_variables()
+                    
+            if st.session_state.noticia_cargada == True:
+                
+                st.success(f"👍🏻 Noticia cargada correctamente. Ahora puedes seguir modificando la noticia más abajo.")
     return
