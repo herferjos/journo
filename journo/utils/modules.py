78927a5abd77d78c22898d47b62b60b55191f5b5
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-13 15:56:41 +0100

    Update modules.py

--- journo/utils/modules.py
+++ journo/utils/modules.py
@@ -21,14 +21,14 @@ openai_client = OpenAI(api_key=st.secrets.openai_api)
 
 def load_database(force=False):
   if 'database' not in st.session_state or force:
-  st.cache_data.clear()
-  st.session_state.sheet = load_sheet()
-  try:
-    st.session_state.database = st.session_state.sheet.read(worksheet=st.session_state.email)
-  except:
-    nuevo_df = pd.DataFrame({'Transcripción': [None]*5, 'Transcripción editada': [None]*5, 'Cargo': [None]*5, 'Nombre': [None]*5, 'Donde': [None]*5, 'Cuando': [None]*5, 'Extra': [None]*5, 'Anotaciones': [None]*5, 'Noticia': [None]*5, 'Noticia editada': [None]*5,'Sesion': [None]*5}, index=range(5))
-    st.session_state.sheet.create(worksheet=st.session_state.email,data=nuevo_df)
-    st.session_state.database = st.session_state.sheet.read(worksheet=st.session_state.email)
+    st.cache_data.clear()
+    st.session_state.sheet = load_sheet()
+    try:
+      st.session_state.database = st.session_state.sheet.read(worksheet=st.session_state.email)
+    except:
+      nuevo_df = pd.DataFrame({'Transcripción': [None]*5, 'Transcripción editada': [None]*5, 'Cargo': [None]*5, 'Nombre': [None]*5, 'Donde': [None]*5, 'Cuando': [None]*5, 'Extra': [None]*5, 'Anotaciones': [None]*5, 'Noticia': [None]*5, 'Noticia editada': [None]*5,'Sesion': [None]*5}, index=range(5))
+      st.session_state.sheet.create(worksheet=st.session_state.email,data=nuevo_df)
+      st.session_state.database = st.session_state.sheet.read(worksheet=st.session_state.email)
   return
 
 def reset_variables():
