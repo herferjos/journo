2675f241535e074550585e8e6a5872fbfc9dfb5a
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-14 21:02:20 +0100

    Update modules.py

--- journo/utils/modules.py
+++ journo/utils/modules.py
@@ -206,16 +206,8 @@ def generar_noticia(declaraciones, anotaciones, X, Y, Z, A, B):
     messages = [
         {"role": "user", "content": f"{prompt} \n Cargo: {X}, Nombre: {Y}, Declaraciones más destacadas: {anotaciones}, 'Contexto': {Z}, Dónde: {A}, Cuándo: {B}. Declaraciones: {declaraciones}."}
     ]
-
-    response_noticia = openai_client.chat.completions.create(
-        model="gpt-4-turbo-preview",
-        messages=messages,
-        temperature=0,
-        seed = 42,
-        stream = True
-    )
   
-    return response_noticia, messages
+    return messages
 
 def extra_noticia(messages):
     messages.append({"role": "user", "content": 'Añade cinco párrafos más a la noticia que cumplan escrupulosamente las indicaciones iniciales, sean coherentes con el resto del texto y no repitan información ya dada. Recuerda que solo puedes citar entre comillas citas exactas del individuo'})
