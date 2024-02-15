6b19a394e1216470de553b16d10abb29b79bd637
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-15 22:11:36 +0100

    Update journo.py

--- journo/pages/journo.py
+++ journo/pages/journo.py
@@ -42,29 +42,7 @@ def show_journo():
                         
         if 'mp3_audio_path' in st.session_state:
             st.audio(st.session_state.mp3_audio_path, format="audio/mpeg")
-            st.success(f"Audio cargado correctamente. Ve a la pestaña de 'Contexto' para continuar")
-
-        with st.expander('**📊 Tus noticias**'):
-            if st.session_state.database.isna().all().all():
-                st.info('Actualmente no has generado ninguna noticia. Adelante, prueba Journo y guarda tu primera noticia asistida por IA')
-    
-                if st.button("Crear nueva noticia", type = "primary", key = "start"):
-                    st.warning('¿Estás seguro de que quieres comenzar a crear una nueva noticia desde cero? Perderás la noticia que estás editando ahora mismo')
-                    if st.button("¡Sí, adelante!", type = "primary", key = "yes"): 
-                        reset_variables()
-            
-            else:
-                st.info('Aquí tienes las noticias que has generado con el asistente Journo. Puedes cargar una noticia directamente, explorar la información o crear una nueva.')
-                df_copia = st.session_state.database.copy()
-                df_copia = df_copia.iloc[:, :-1]
-                st.session_state.index_cargado = dataframetipo(df_copia)
-    
-                if st.button("Cargar noticia seleccionada", type = "primary", key = "start"):
-                    cargar_noticia()
-                        
-                if st.session_state.noticia_cargada == True:
-                    
-                    st.success(f"👍🏻 Noticia cargada correctamente. Ahora puedes seguir modificando la noticia más abajo.")      
+            st.success(f"Audio cargado correctamente. Ve a la pestaña de 'Contexto' para continuar")   
     
     if st.session_state.phase == 1:
         st.info(f"Una vez acabes de rellenar los campos, ve a la pestaña de 'Transcripción' para continuar")
