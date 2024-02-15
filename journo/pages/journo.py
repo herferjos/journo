2cb48a4a09d2e07a8ad95210186673f562268743
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-15 22:32:19 +0100

    Update journo.py

--- journo/pages/journo.py
+++ journo/pages/journo.py
@@ -127,6 +127,7 @@ def show_journo():
                 st.session_state.messages = generar_noticia(st.session_state.transcripcion_editada, st.session_state.anotaciones_finales, st.session_state.X, st.session_state.Y, st.session_state.Z, st.session_state.A, st.session_state.B)
 
         with st.container(height = min(len(st.session_state.messages) * 150, 500)):
+            st.write(st.session_state.generacion)
             for i in range(len(st.session_state.messages)):
                 if st.session_state.messages[i]["role"] == "system" or i == 1:
                     pass
