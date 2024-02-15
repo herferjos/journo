14450c4b9f393bceed1fe3670bb9ba220083423b
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-15 14:50:58 +0100

    Update __init__.py

--- journo/components/boton_stripe/__init__.py
+++ journo/components/boton_stripe/__init__.py
@@ -1,5 +1,5 @@
 import streamlit.components.v1 as components
 boton_stripe = components.declare_component(
     "boton_stripe",
-    path="./boton_stripe"
+    path="./journo/components/boton_stripe"
 )
