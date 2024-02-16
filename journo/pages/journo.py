a299650fdc576ff37f5d5108a8a3993dbde08e18
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-16 18:19:17 +0100

    Update journo.py

--- journo/pages/journo.py
+++ journo/pages/journo.py
@@ -78,7 +78,7 @@ def show_journo():
                                   for x in item:
                                     frases.append(x['label'])
                               
-                                st.write(generar_html_con_destacados(st.session_state.lista[i], frases), unsafe_allow_html=True)
+                              st.write(generar_html_con_destacados(st.session_state.lista[i], frases), unsafe_allow_html=True)
 
 
         if 'transcription2' in st.session_state:
