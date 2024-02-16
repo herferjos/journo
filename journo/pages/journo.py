079dbb4cc91f60cc963d524c7272fa85c878e60a
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-16 17:26:14 +0100

    Update journo.py

--- journo/pages/journo.py
+++ journo/pages/journo.py
@@ -127,44 +127,44 @@ def show_journo():
                 st.session_state.messages = generar_noticia(st.session_state.transcripcion_editada, st.session_state.anotaciones_finales, st.session_state.X, st.session_state.Y, st.session_state.Z, st.session_state.A, st.session_state.B)
                 st.session_state.generacion = True
                 st.rerun()
-
-        with st.container(height = min(len(st.session_state.messages) * 150, 500)):
-            for i in range(len(st.session_state.messages)):
-                if st.session_state.messages[i]["role"] == "system" or i == 1:
-                    pass
-                else:
-                    with st.chat_message(st.session_state.messages[i]["role"]):
-                        st.markdown(st.session_state.messages[i]["content"])
-            
-            if prompt := st.chat_input("Pregunta lo que quieras") or st.session_state.generacion:
-    
-                if st.session_state.generacion == False:
+        if len(st.session_state.messages) > 0:
+            with st.container(height = min(len(st.session_state.messages) * 150, 500)):
+                for i in range(len(st.session_state.messages)):
+                    if st.session_state.messages[i]["role"] == "system" or i == 1:
+                        pass
+                    else:
+                        with st.chat_message(st.session_state.messages[i]["role"]):
+                            st.markdown(st.session_state.messages[i]["content"])
                 
-                    st.session_state.messages.append({"role": "user", "content": prompt})
-                
-                    with st.chat_message("user"): 
-                        st.markdown(prompt)
-                
-                with st.chat_message("assistant"):
-   
-                    response = openai_client.chat.completions.create(
-                    model="gpt-4-turbo-preview",
-                    messages=st.session_state.messages,
-                    temperature = 0,
-                    stream = True
-                    )
-                    
-                    message_placeholder = st.empty()
-                    full_response = ""
-                    
-                    for chunk in response:
-                        if chunk.choices[0].delta.content is not None:
-                            full_response += chunk.choices[0].delta.content
-                            message_placeholder.markdown(full_response + "▌")
+                if prompt := st.chat_input("Pregunta lo que quieras") or st.session_state.generacion:
         
-                              
-                    st.session_state.messages.append({"role": "assistant", "content": full_response})
-                    st.session_state.generacion = False
-                    st.rerun()
+                    if st.session_state.generacion == False:
+                    
+                        st.session_state.messages.append({"role": "user", "content": prompt})
+                    
+                        with st.chat_message("user"): 
+                            st.markdown(prompt)
+                    
+                    with st.chat_message("assistant"):
+       
+                        response = openai_client.chat.completions.create(
+                        model="gpt-4-turbo-preview",
+                        messages=st.session_state.messages,
+                        temperature = 0,
+                        stream = True
+                        )
+                        
+                        message_placeholder = st.empty()
+                        full_response = ""
+                        
+                        for chunk in response:
+                            if chunk.choices[0].delta.content is not None:
+                                full_response += chunk.choices[0].delta.content
+                                message_placeholder.markdown(full_response + "▌")
+            
+                                  
+                        st.session_state.messages.append({"role": "assistant", "content": full_response})
+                        st.session_state.generacion = False
+                        st.rerun()
 
     return
