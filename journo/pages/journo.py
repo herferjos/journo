ffefcf7ae65af5a767ebc35ac0880e9cb77f5b6e
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-16 18:21:12 +0100

    Update journo.py

--- journo/pages/journo.py
+++ journo/pages/journo.py
@@ -114,15 +114,17 @@ def show_journo():
             st.write("""## ✅ ¡Ya está lista tu noticia!""")
             with st.expander('Editar noticia original'):
                 st.session_state.noticia_editada = st.text_area(label = ":blue[Noticia generada]", value = st.session_state.noticia_editada, height = int(len(st.session_state.noticia_editada)/5))
-            
-            if st.button("Guardar noticia", type = "primary"): 
-               st.session_state.messages.append({"role": "user", "content": f'Esta es la nueva noticia editada por mi: {st.session_state.noticia_editada}'})
+            a,b = st.columns([0.7,1])
+            with a:
+                if st.button("Guardar noticia", type = "primary"): 
+                   st.session_state.messages.append({"role": "user", "content": f'Esta es la nueva noticia editada por mi: {st.session_state.noticia_editada}'})
                 
-            if st.button("Volver a generar noticia", type = "primary"): 
-              with st.spinner("Generando noticia... ⌛"):
-                st.session_state.messages = generar_noticia(st.session_state.transcripcion_editada, st.session_state.anotaciones_finales, st.session_state.X, st.session_state.Y, st.session_state.Z, st.session_state.A, st.session_state.B)
-                st.session_state.generacion = True
-                st.rerun()
+            ith b:
+                if st.button("Volver a generar noticia", type = "primary"): 
+                  with st.spinner("Generando noticia... ⌛"):
+                    st.session_state.messages = generar_noticia(st.session_state.transcripcion_editada, st.session_state.anotaciones_finales, st.session_state.X, st.session_state.Y, st.session_state.Z, st.session_state.A, st.session_state.B)
+                    st.session_state.generacion = True
+                    st.rerun()
 
         else:
             st.warning('Aún no has generado ninguna noticia, dale click a "Generar noticia"')
