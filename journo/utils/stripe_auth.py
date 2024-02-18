6cc43dc2d3defd52a3e1d43fab606f6c04ca9521
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-18 14:34:02 +0100

    Update stripe_auth.py

--- journo/utils/stripe_auth.py
+++ journo/utils/stripe_auth.py
@@ -50,7 +50,7 @@ def redirect_button(
         .pago-button {
             background-color: #ffffff;
             color: #000000;
-            padding: 8px 15px;
+            padding: 4px 15px;
             border: 2px solid #cccccc;
             border-radius: 5px;
             font-weight: bold;
