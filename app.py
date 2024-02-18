16694a7d436e9cbc9e1cdb62fb5d1134981edaea
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-18 13:51:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ st.markdown("""
     .google-icon {
         margin-right: 300px;
         fill: #000000;
-        width: 20px;
-        height: 20px;
+        width: 0px;
+        height: 0px;
     }
     </style>
 """, unsafe_allow_html=True)
