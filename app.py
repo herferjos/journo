b0573f260d673c2edd01695500169679345be04d
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-18 13:58:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,46 +7,6 @@ import time
 
 st.set_page_config(page_title="Journo", page_icon="🗞️") #layout="wide"
 
-# Muestra el estilo del botón de Google
-st.markdown("""
-    <style>
-    .google-button {
-        background-color: #ffffff;
-        color: #000000;
-        padding: 10px 20px;
-        border: 2px solid #cccccc;
-        border-radius: 5px;
-        display: inline-flex;
-        align-items: center;
-        justify-content: center;
-        font-weight: bold;
-        cursor: pointer;
-        text-decoration: none;
-    }
-    .google-icon {
-        margin-right: 10px;
-    }
-    .google-button:hover {
-        border-color: #000000;
-    }
-    .google-text {
-        margin-left: 10px;
-    }
-    </style>
-""", unsafe_allow_html=True)
-
-# Muestra el botón de Google con el SVG cargado
-st.markdown(
-    f"""
-    <a href="http://www.google.com" class="google-button">
-        <span class="google-icon">
-            {svg_content}
-        </span>
-        <span class="google-text">Iniciar Sesión con Google</span>
-    </a>
-    """,
-    unsafe_allow_html=True
-)
 
 # Lee el contenido del archivo SVG
 with open("files/google.svg", "r") as file:
