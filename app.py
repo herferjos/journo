43a9fbd6743f92e6db368f0a863bd7641ac4dc00
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-18 13:53:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ with open("files/google.svg", "r") as file:
     svg_content = file.read()
 
 # Modificar el tamaño del SVG
-svg_content = svg_content.replace('<svg ', '<svg width="10" height="10" ')
+svg_content = svg_content.replace('<svg ', '<svg width="20" height="20" ')
 
 # Muestra el estilo del botón de Google
 st.markdown("""
