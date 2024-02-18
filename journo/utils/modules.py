db2bad4b4d039408cd8d478b722caaa7db6e2dfb
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-18 20:35:33 +0100

    Update modules.py

--- journo/utils/modules.py
+++ journo/utils/modules.py
@@ -150,8 +150,7 @@ def transcribe_audio_2(file_path):
     if 'model' not in st.session_state:
       st.session_state.model = load_model()
     
-    segments, info = st.session_state.model.transcribe(file_path, beam_size=5, 
-        language="es", condition_on_previous_text=False)
+    segments, info = st.session_state.model.transcribe(file_path, beam_size=5,language="es", condition_on_previous_text=False, vad_filter=True, vad_parameters=dict(min_silence_duration_ms=500))
         
     return segments
   
