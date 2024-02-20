0a2fdf44372367418819fc1e97c2f72bf0573f68
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-20 12:37:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ openai_client = OpenAI(api_key=st.secrets.openai_api)
 
 st.set_page_config(page_title="Journo", page_icon="🗞️")
 
-for key, value in anotaciones_vars.items():
+for key, value in {key: value for key, value in st.session_state.items() if key.startswith('anotaciones')}.items():
     st.write(f"{key}: {value}")
 
 st.markdown(
