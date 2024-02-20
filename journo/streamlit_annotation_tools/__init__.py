7015f9ecffebb877e0782aa4c11ec217f0a854a6
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-20 15:59:22 +0100

    Update __init__.py

--- journo/streamlit_annotation_tools/__init__.py
+++ journo/streamlit_annotation_tools/__init__.py
@@ -2,16 +2,10 @@ import os
 
 import streamlit.components.v1 as components
 
-RELEASE = os.environ.get("STREAMLIT_ANNOTATION_TOOLS_RELEASE", "true") == "true"
 
-if not RELEASE:
-    _component = components.declare_component(
-        "annotation_tools", url="http://localhost:3001"
-    )
-else:
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    _component = components.declare_component("annotation_tools", path=build_dir)
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/build")
+_component = components.declare_component("annotation_tools", path='/mount/src/journo/journo/streamlit_annotation_tools/frontend/src/component/Builder.tsx')
 
 
 def text_highlighter(text: str, highlights=[]):
