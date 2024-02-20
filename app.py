521fc13e26560c62ffedba580d02a41f844fc581
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-20 12:33:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -161,9 +161,12 @@ if 'email' in st.session_state and st.session_state.user_subscribed == True:
 
             for i in range(len(st.session_state.lista)):
                 if f'anotaciones_{i}' not in st.session_state:
-                    st.session_state[f'anotaciones_{i}'] = [[{},{}],[{},{}]]
-                st.session_state[f'anotaciones_{i}'] = text_highlighter(st.session_state.lista[i], st.session_state[f'anotaciones_{i}'])
-            
+                    st.session_state[f'anotaciones_{i}'] = None
+                if st.session_state[f'anotaciones_{i}'] == None:
+                    st.session_state[f'anotaciones_{i}'] = text_highlighter(st.session_state.lista[i])
+                else:
+                    st.session_state[f'anotaciones_{i}'] = text_highlighter(st.session_state.lista[i], st.session_state[f'anotaciones_{i}'])
+
         else:
             st.warning('Aún no has generado ninguna transcripción. Vuelve al paso de contexto y guarda la información para que la transcripción se genere correctamente.')
 
