4d5cbeadf3c504db7defae3905289e4af31845cb
herferjos <120600448+herferjos@users.noreply.github.com>
2024-02-26 18:33:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -134,51 +134,50 @@ if 'email' in st.session_state and st.session_state.user_subscribed == True:
                         cargar_y_transcribir_audio(audio['bytes'])
                         
         if 'mp3_audio_path' in st.session_state:
-
-            # Style buttons as links
-            with stylable_container(
-                key="link_buttons",
-                css_styles="""
-                button {
-                    background: none!important;
-                    border: none;
-                    padding: 0!important;
-                    font-family: arial, sans-serif;
-                    color: #069;
-                    text-decoration: underline;
-                    cursor: pointer;
-                }
-                """,
-            ):
-                timestamps = st.session_state.timestamps
-                num_timestamps = len(timestamps)
-                for i in range(0, num_timestamps, 3):
-                    group = timestamps[i:i+3]  # Obtener un grupo de tres elementos
-                    start = int(group[0]['start'])  # Obtener el tiempo inicial del primer elemento
-                    end = int(group[-1]['end'])  # Obtener el tiempo final del último elemento
-            
-                    # Convertir los tiempos en minutos y segundos
-                    minuto_start = start // 60
-                    segundo_start = start % 60
-                    start_text = f"{minuto_start:02d}:{segundo_start:02d}"
-            
-                    minuto_end = end // 60
-                    segundo_end = end % 60
-                    end_text = f"{minuto_end:02d}:{segundo_end:02d}"
-                    
-                    range = f"{start_text} - {end_text}"
-            
-                    # Mostrar el rango de tiempo como un botón
-                    if st.button(range):
-                        st.session_state["start_time"] = start
-                        st.rerun()
-            
-                    # Mostrar el texto de cada elemento en el grupo
-                    for timestamp in group:
-                        st.write(timestamp['text'])
-            
-            # Añadir la reproducción de audio al final del bucle de tiempo
-            st.audio(st.session_state.mp3_audio_path, format="audio/mpeg", start_time=st.session_state.start_time)
+            with st.container(height = 200, border = True):
+                with stylable_container(
+                    key="link_buttons",
+                    css_styles="""
+                    button {
+                        background: none!important;
+                        border: none;
+                        padding: 0!important;
+                        font-family: arial, sans-serif;
+                        color: #069;
+                        text-decoration: underline;
+                        cursor: pointer;
+                    }
+                    """,
+                ):
+                    timestamps = st.session_state.timestamps
+                    num_timestamps = len(timestamps)
+                    for i in range(0, num_timestamps, 3):
+                        group = timestamps[i:i+3]  # Obtener un grupo de tres elementos
+                        start = int(group[0]['start'])  # Obtener el tiempo inicial del primer elemento
+                        end = int(group[-1]['end'])  # Obtener el tiempo final del último elemento
+                
+                        # Convertir los tiempos en minutos y segundos
+                        minuto_start = start // 60
+                        segundo_start = start % 60
+                        start_text = f"{minuto_start:02d}:{segundo_start:02d}"
+                
+                        minuto_end = end // 60
+                        segundo_end = end % 60
+                        end_text = f"{minuto_end:02d}:{segundo_end:02d}"
+                        
+                        range = f"{start_text} - {end_text}"
+                
+                        # Mostrar el rango de tiempo como un botón
+                        if st.button(range):
+                            st.session_state["start_time"] = start
+                            st.rerun()
+                
+                        # Mostrar el texto de cada elemento en el grupo
+                        for timestamp in group:
+                            st.write(timestamp['text'])
+                
+                # Añadir la reproducción de audio al final del bucle de tiempo
+                st.audio(st.session_state.mp3_audio_path, format="audio/mpeg", start_time=st.session_state.start_time)
 
 
 
