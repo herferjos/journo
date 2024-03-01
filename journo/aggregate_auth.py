8a70baf844776d5cc366f5b4c04ef51bc1a8c704
herferjos <120600448+herferjos@users.noreply.github.com>
2024-03-01 18:07:15 +0100

    Update aggregate_auth.py

--- journo/aggregate_auth.py
+++ journo/aggregate_auth.py
@@ -16,8 +16,17 @@ def auth():
             cabecera()
             show_login_button()
         with xf:
-            st.write(f'<div style="display: flex; justify-content: center;"><video width="1000" height="600" controls loop autoplay><source src="data:video/mp4;base64,{base64.b64encode(open("files/Journo Demo.mp4", "rb").read()).decode()}" type="video/mp4"></video></div>', unsafe_allow_html=True)
-
+            with open("files/video_base64.txt", "r") as input_file:
+                video_base64 = input_file.read()
+            
+            # Muestra el video utilizando la representación base64
+            st.write(f'''
+                <div style="display: flex; justify-content: center;">
+                    <video width="1000" height="600" controls loop autoplay>
+                        <source src="data:video/mp4;base64,{video_base64}" type="video/mp4">
+                    </video>
+                </div>
+            ''', unsafe_allow_html=True)
         
         #show_inicio2()
         
