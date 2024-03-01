5e27f71ad21bebb04701bbb94b289e4db5b2d467
herferjos <120600448+herferjos@users.noreply.github.com>
2024-03-01 13:21:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,20 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+footer="
+
+<style> your css code put here</style>
+
+<div class='footer'>
+
+<p>the word you want to tell<a style='display:block;text-align:center;' 
+
+href='https://www.streamlit.io' target='_blank'>your email address put here</a></p>
+
+</div>"
+
+st.markdown(footer, unsafe_allow_html=True)
+
 st.markdown(
     """
     <style>
