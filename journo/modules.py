e5ebb5f9ff521f42290131cd4b2aef462905bc19
herferjos <120600448+herferjos@users.noreply.github.com>
2024-03-01 16:21:42 +0100

    Update modules.py

--- journo/modules.py
+++ journo/modules.py
@@ -329,7 +329,9 @@ def listas_iguales(lista1, lista2):
 
 
 def show_inicio():
-  st.video('files/Journo Demo.mp4')
+  st.write("## 🤔 ¿Qué es Journo?")
+  with st.container(border=True):
+    st.video('files/Journo Demo.mp4')
   return
   
 def show_inicio2():
