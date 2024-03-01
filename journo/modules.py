3387a4ce3360998ed43fa2df3625a98234001fce
herferjos <120600448+herferjos@users.noreply.github.com>
2024-03-01 19:55:20 +0100

    Update modules.py

--- journo/modules.py
+++ journo/modules.py
@@ -355,7 +355,9 @@ def show_inicio():
       #
 
       <p style="font-size: 15px;">Hecho con ❤️ desde Málaga. Por y para periodistas.</p>
-    
+
+      <p> </p>
+      
       </div>
       
     </div>
